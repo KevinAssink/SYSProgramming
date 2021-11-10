fd881eb0a5c65f6ff144a409bbb5be1efb659a7a
KevinAssink <31471104+KevinAssink@users.noreply.github.com>
2021-11-10 19:05:12 +0100

    added some lessons i made

---
+++ les2/Les2Classes/Les2Classes/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Les2Classes
+{
+	class Enemy
+	{
+		public int HP = 50; 
+		public float moveSpeed = 3f;
+		public int Damage = 1;
+		
+	}
+}
